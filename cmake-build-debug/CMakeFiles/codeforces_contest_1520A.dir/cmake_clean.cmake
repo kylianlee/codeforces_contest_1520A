af20bbed9bdaa5642f657fe7319e123b074db912
Kylian <hsjgc1234@gmail.com>
2021-06-22 12:06:09 +0900

    solution

---
+++ cmake-build-debug/CMakeFiles/codeforces_contest_1520A.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codeforces_contest_1520A.dir/main.cpp.o"
+  "codeforces_contest_1520A"
+  "codeforces_contest_1520A.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codeforces_contest_1520A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/codeforces_contest_1520A.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for codeforces_contest_1520A.
+# This may be replaced when dependencies are built.
